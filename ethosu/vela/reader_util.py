nxp-imx/ethos-u-vela

commit 0af0d383925968626a7c37b084ad806742511e79

MLBEDSW-4704: Vela crashes when serializing subset of Inception v3 network
* Add check for tensor with no operations, raising error if its constant-data buffer is empty

Signed-off-by: Alex Matthews <alex.matthews@arm.com>
Change-Id: Ib210dcc9733e4ecedbada0f430e8b3c4a8384999
Signed-off-by: James Ward <james.ward@arm.com>

ethosu/vela/reader_util.py
@@ -53,6 +53,11 @@ def fixup_tensors(input_tensors, tensors):
 
     for tens in tensors:
         if not tens.ops:
+            if tens.values is None:
+                tens.error(
+                    "Tensor with no operator output connection is assumed to contain constant "
+                    "data but no data buffer is associated with this tensor. Perhaps the network is invalid?"
+                )
             op = Operation(Op.Const, tens.name)
             op.set_output_tensor(tens)
 

commit e6607c563f3ea480fdc25770fdd7434ec7ab776b

MLBEDSW-5496: Revert patch for MLBEDSW-4704
This reverts commit 0af0d383925968626a7c37,
which caused a regression by rejecting
previously passing tests as faulty.

Signed-off-by: Dwight Lidman <dwight.lidman@arm.com>
Change-Id: If11737713b6873a67162387e407eadf174b434ec

ethosu/vela/reader_util.py
@@ -53,11 +53,6 @@ def fixup_tensors(input_tensors, tensors):
 
     for tens in tensors:
         if not tens.ops:
-            if tens.values is None:
-                tens.error(
-                    "Tensor with no operator output connection is assumed to contain constant "
-                    "data but no data buffer is associated with this tensor. Perhaps the network is invalid?"
-                )
             op = Operation(Op.Const, tens.name)
             op.set_output_tensor(tens)
 