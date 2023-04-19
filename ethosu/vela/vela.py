5574d192fa352b3f27b842de1d551d3f113eca32
Feng Guo <feng.guo@nxp.com>
2023-04-19 18:06:28 +0800

    Fix vela memory leak issue
    Signed-off-by: Feng Guo <feng.guo@nxp.com>

--- ethosu/vela/vela.py
+++ ethosu/vela/vela.py
@@ -363,6 +363,7 @@ def convert(input_model_name):
 
     output_tfl_filename = output_basename + "_vela.tflite"
     tflite_writer.write_tflite(nng, output_tfl_filename)
+    DebugDatabase.clean_db()
 
     return output_tfl_filename
 
@@ -397,8 +398,9 @@ def convert_bytes(data):
         raise InputFileError(input_model_name, "Invalid data")
 
     compiler_driver.compiler_driver(nng, arch, compiler_options, scheduler_options, network_type, "data_model")
-
     buf = tflite_writer.write_tflite_buffer(nng)
+    DebugDatabase.clean_db()
+
     return memoryview(buf)
 
 def main(args=None):
